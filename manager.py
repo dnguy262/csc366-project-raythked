9dfc23096069b1e722c3e3d5775093f8a71e1a54
jcavalca <jcavalca@calpoly.edu>
2022-05-19 10:25:04 -0700

    More changes, gotta test DB change

--- manager.py
+++ manager.py
@@ -1,26 +1,29 @@
 import argparse
 import pymysql
 
+# This file manages the creation and deletion of tables
+
 def executeQuery(query):
-    # connection = pymysql.connect(
-    #             user     = "group5a",
-    #             password = "_}2nSW6'%?3hyr9Z",
-    #             host     = "https://labthreesixsix.com",
-    #             db       = "group5a",
-    # )
     connection = pymysql.connect(
-        user     = "jcavalca466",
-        password = "jcavalca466985",
-        host     = "localhost",
-        db       = "jcavalca466",
-        port     = 9090 # comment out this if running on frank
+                user     = "group5a",
+                password = "_}2nSW6'%?3hyr9Z",
+                host     = "mysql.labthreesixfive.com",
+                db       = "group5a",
+                port     = 3306
     )
+    # connection = pymysql.connect(
+    #     user     = "jcavalca466",
+    #     password = "jcavalca466985",
+    #     host     = "localhost",
+    #     db       = "jcavalca466",
+    #     port     = 9090 # comment out this if running on frank
+    # )
     with connection.cursor() as cursor:
         cursor.execute(query)
     connection.commit()
     connection.close()
   
-    
+# Creates all tables
 def createTables():
     with open("DB-setup-raythked.sql", 'r') as file:
         queries = [line.strip('\n') for line in file.readlines()]
@@ -28,6 +31,7 @@ def createTables():
         for query in queries:
             executeQuery(query)
 
+# Drops all tables
 def dropTables():
     with open("DB-cleanup-raythked.sql", 'r') as file:
         queries = [line.strip('\n') for line in file.readlines()]
@@ -35,6 +39,7 @@ def dropTables():
         for query in queries:
             executeQuery(query)
 
+# Driver function, have to run either with -c or -d
 def main():
     parser = argparse.ArgumentParser(description='Creates all DB tables if any does not exist.')
     parser.add_argument('-c', action='store_true')
--- populate.py
+++ populate.py
@@ -1,26 +1,44 @@
 import subprocess
+from os.path import exists
 from manager import executeQuery
+
+# This file populates our DB w/ excel and ONET data.
 
 def main():
 
-    # Creates tables, if necessary
+    # Drop tables (we don't support updates yet)
+    subprocess.run("python3 manager.py -d".split())
+
+    # Creates tables
     subprocess.run("python3 manager.py -c".split())
 
-    # Creates files w/ insert stmts
-    subprocess.run("python3 parse.py".split())
+    filenames = ['./insert_stms_excel.sql', './onet-parse/sql/onet-job-info.sql', './onet-parse/sql/onet-job-profiles.sql']
+
+    # Creates insert file from excel, if doesn't exists
+    if not exists(filenames[0]):
+        subprocess.run("python3 parse.py".split())
+
+     # Creates insert file from ONET, if doesn't exists
+    if not exists(filenames[1]) or not exists(filenames[2]):
+        subprocess.run("cd onet-parse && yarn run parse:script".split())
 
     # Executing insert stmts
-    filenames = ['./insert_stms_excel.sql']
-
     for filename in filenames:
         with open(filename, 'r') as file:
+            
             queries = [q.strip('\n') for q in file.readlines()]
+            
+            # Handling another format for the file
+            if 'onet' in filename:
+                queries = [' '.join(queries)]
+
             for query in queries:
                 try:
                     executeQuery(query)
                 except:
                     print(query)
                     executeQuery(query)
-                    
+
+            print(f"Done inserting from {filename}")
 if __name__ == '__main__':
     main()
