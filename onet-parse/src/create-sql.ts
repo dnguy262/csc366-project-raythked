dcfd89c192d44803e17f907bc0e84e2b422a7dee
Jin Choi <jinyoungchoi118@gmail.com>
2022-05-18 18:16:22 -0700

    added logic for parsing the ONET data into the appropriate SQL statements

--- onet-parse/src/create-sql.ts
+++ onet-parse/src/create-sql.ts
@@ -8,18 +8,21 @@ import { ONETJobDetailResponse } from './types';
  * Creates the following sql files:
  * - onet-job-profiles.sql 
  * - onet-job-info.sql
- * - recommended-job-list.sql
  */
 export const createSql = (data: Array<ONETJobDetailResponse>) => {
-    let onetJobInfoInserts = '';
-    let onetJobProfilesInserts = '';
-    let recomnededJobListInserts = '';
-    // TODO: finalize create table statements so the insert statement makes sense
-    // onet job info is pretty straight forward
-    // onet job profile needs some way to map back to onet-job-info
-    // recommended-job-list I am not sure on
+    let onetJobInfoInserts = 'INSERT INTO ONETJobInfo(Code, Title, IsStem, Description, BrightOutlook, Green) \nVALUES';
+    let onetJobProfilesInserts = 'INSERT INTO Profile(Name, Category, Code) \nVALUES';
 
-    fs.writeFileSync('onet-job-info.sql', onetJobInfoInserts);
-    fs.writeFileSync('onet-job-profiles.sql', onetJobProfilesInserts);
-    fs.writeFileSync('recommended-job-list.sql', recomnededJobListInserts);
+    data.forEach(({ code, description, tags, title }, index) => {
+        const { bright_outlook, green } = tags;
+        // escapes any single quotes in the description 
+        const escapedDescription = description.replace(/'/g, "''");
+        onetJobInfoInserts += `\n('${code}', '${title}', true, '${escapedDescription}', ${bright_outlook}, ${green})`;
+        onetJobProfilesInserts += `\n('${title}', 'ONET', '${code}')`;
+        onetJobInfoInserts += index === data.length - 1 ? ';' : ',';
+        onetJobProfilesInserts += index === data.length - 1 ? ';' : ',';
+    });
+
+    fs.writeFileSync('sql/onet-job-info.sql', onetJobInfoInserts);
+    fs.writeFileSync('sql/onet-job-profiles.sql', onetJobProfilesInserts);
 }
