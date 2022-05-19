81e270e3a5729c3bfec1df75b76d7416ee47853e
jcavalca <jcavalca@calpoly.edu>
2022-05-18 19:12:28 -0700

    Redid create and drop stmts

---
+++ DB-cleanup-raythked.sql
@@ -0,0 +1,4 @@
+DROP TABLE if exists JobsToJobList,RecommendedJobLists,ONETJobInfo,
+	Responses, DescriptorScores, SurveySubmissions,
+	Profiles, UserAccounts, Choices, 
+	Questions, JobDescriptors, Surveys;
