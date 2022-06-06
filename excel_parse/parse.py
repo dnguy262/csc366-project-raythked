1de2dda89bd5572e7dcb99bf250b68ba25b7e863
jcavalca <jcavalca@calpoly.edu>
2022-06-05 23:31:38 -0700

    Fixing missing choices for questions 12-94

--- excel_parse/parse.py
+++ excel_parse/parse.py
@@ -1,39 +1,53 @@
 import openpyxl
- 
+
+
 def parseProfileCharacteristics():
-    path = './ProfileCharacteristics.xlsx'
- 
-    wb_obj = openpyxl.load_workbook(path) # To open the workbook, workbook object is created
-    sheet_obj = wb_obj.active # Get workbook active sheet object from the active attribute
-    
+    path = "./ProfileCharacteristics.xlsx"
+
+    wb_obj = openpyxl.load_workbook(
+        path
+    )  # To open the workbook, workbook object is created
+    sheet_obj = (
+        wb_obj.active
+    )  # Get workbook active sheet object from the active attribute
+
     max_col = sheet_obj.max_column
     max_row = sheet_obj.max_row
     queryList = []
-    for row in range(2, max_row + 1): # skip column titles
-        
+    for row in range(2, max_row + 1):  # skip column titles
+
         rowValues = []
         for col in range(1, max_col + 1):
-            cell_obj = sheet_obj.cell(row = row, column = col)
+            cell_obj = sheet_obj.cell(row=row, column=col)
             rowValues.append(str(cell_obj.value))
 
-        query = ("""INSERT INTO JobDescriptors (Id, Dimension, Characteristic, Description) """
-                """VALUES ({}, "{}", "{}", "{}");""".format(rowValues[0], rowValues[1], rowValues[2], rowValues[3]))
+        query = (
+            """INSERT INTO JobDescriptors (Id, Dimension, Characteristic, Description) """
+            """VALUES ({}, "{}", "{}", "{}");""".format(
+                rowValues[0], rowValues[1], rowValues[2], rowValues[3]
+            )
+        )
 
         queryList.append(query)
 
         rowValues.clear()
     return queryList
 
+
 def parseWorksheet():
     # Definition Worksheet
-    path = './Data-v03.xlsx'
-    wb_obj = openpyxl.load_workbook(path) # To open the workbook, workbook object is created
-    
+    path = "./Data-v03.xlsx"
+    wb_obj = openpyxl.load_workbook(
+        path
+    )  # To open the workbook, workbook object is created
+
     # Aggregating insert queries by worksheet
     queries = {}
     for worksheet in wb_obj.sheetnames:
-        sheet_obj = wb_obj[worksheet] # Get workbook active sheet object from the active attribute
-        
+        sheet_obj = wb_obj[
+            worksheet
+        ]  # Get workbook active sheet object from the active attribute
+
         if worksheet == "URE Experience":
             parseUREExperienceSheet(sheet_obj, queries)
 
@@ -41,17 +55,17 @@ def parseWorksheet():
             max_col = sheet_obj.max_column
             max_row = sheet_obj.max_row
 
-            for row in range(2, max_row + 1): # skip column titles
-                
+            for row in range(2, max_row + 1):  # skip column titles
+
                 rowValues = []
                 for col in range(1, max_col + 1):
-                    cell_obj = sheet_obj.cell(row = row, column = col)
+                    cell_obj = sheet_obj.cell(row=row, column=col)
                     rowValues.append(str(cell_obj.value))
-                
+
                 # Some worksheets start in 2nd or 3rd row, this will catch empty rows
-                if rowValues[0] == 'None':
+                if rowValues[0] == "None":
                     continue
-                
+
                 query = ""
                 if worksheet == "Surveys":
                     query = f"""INSERT INTO Surveys (Title, Name, Description) VALUES ("{rowValues[1]}", "{rowValues[2]}", "{rowValues[3]}");"""
@@ -67,16 +81,35 @@ def parseWorksheet():
 
                 # Replacing Python's None to MySQL's Null for missing fields
                 query = query.replace('"None"', "Null")
-                query = query.replace('None', "Null")
-                
+                query = query.replace("None", "Null")
+
                 if worksheet not in queries.keys():
                     queries[worksheet] = []
-                
+
                 # There are still other worksheets to be implemented
                 if query:
                     queries[worksheet].append(query)
 
+    # Hardcoding Missing Choices from Excel (12-94) have same choices
+    cnumbers = range(8)
+    cvalues = [
+        "Prefer not to answer",
+        "strongly disagree",
+        "disagree",
+        "somewhat disagree",
+        "neither agree nor disagree",
+        "somewhat agree",
+        "agree",
+        "strongly agree",
+    ]
+    questions_missing = range(13, 95)
+    survey_id = 1 # URE Survey
+    for qnumber in questions_missing:
+        for cnumber, cvalue in zip(cnumbers, cvalues):
+            query = f"""INSERT INTO Choices (SurveyId, QNumber, CNumber, Value) VALUES ({survey_id}, {qnumber}, {cnumber}, "{cvalue}");"""
+            queries["QuestionResponses"].append(query)
     return queries
+
 
 # RESOURCES
 # https://www.geeksforgeeks.org/python-reading-excel-file-using-openpyxl-module/
@@ -85,6 +118,8 @@ def parseWorksheet():
 """
     This function updates the queries dictionary, and does not return anything
 """
+
+
 def parseUREExperienceSheet(sheet_obj, queries):
 
     # initialize sheet queries list
@@ -95,18 +130,19 @@ def parseUREExperienceSheet(sheet_obj, queries):
     max_col = 96
     max_row = 27
 
-    #None as of now... need to create surveySubmissions (?)
+    # None as of now... need to create surveySubmissions (?)
     surveySubmissionId = "Null"
     for row in range(3, max_row + 1):
         rowValues = []
         questionId = 1
         query = ""
         for col in range(3, max_col + 1):
-            cell_obj = sheet_obj.cell(row = row, column = col)
+            cell_obj = sheet_obj.cell(row=row, column=col)
             rowValues.append(str(cell_obj.value))
             query = f"""INSERT INTO Responses (SurveySubmissionId, QuestionId, SelectedChoiceId) VALUES ({surveySubmissionId}, {questionId}, {cell_obj.value});"""
             questionId += 1
             queries["URE Experience"].append(query)
+
 
 # def parseUREExperienceSheet(sheet_obj, queries)
 
@@ -170,18 +206,20 @@ def parseUREExperienceSheet(sheet_obj, queries):
 #     connection.commit()
 #     connection.close()
 
+
 def main():
     definitions_queries = parseProfileCharacteristics()
     queries = parseWorksheet()
-    
+
     all_queries = definitions_queries
     for queries in queries.values():
         all_queries.extend(queries)
-    
+
     all_queries = [q + "\n" for q in all_queries]
-    
-    with open('insert_stms_excel.sql', 'w') as file:
+
+    with open("insert_stms_excel.sql", "w") as file:
         file.writelines(all_queries)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
