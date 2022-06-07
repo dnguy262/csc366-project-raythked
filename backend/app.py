b3d03658abe3888de34b5dfb45c61cb6b81dcc4d
dnguy262 <49251040+dnguy262@users.noreply.github.com>
2022-06-06 19:57:07 -0700

    Added matching algorithm functionality
    Returns recommended job list. Needs testing

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
+from pyparsing import identchars
 from flask import Flask, jsonify, request
+from scipy import spatial
+from operator import itemgetter
 import json 
-
+import pymysql
 import db
 
 app = Flask(__name__)
@@ -58,10 +61,17 @@ def get_surveys():
 
 @app.route("/api/survey", methods=['POST'])
 def post_survey():
+    
     # TODO : Test w/ real data from frontend
-    # data = request.get_json()
-    # survey_id = int(data['survey_id'])
-    survey_id = 1
+    request_data = request.get_json()
+    # survey_id = 1
+    survey_id = int(request_data['survey_id'])
+    resultObjList = request_data['results'] # [{qnumber, cnumber}, {qnumber, cnumber}]
+    qnumbers = []
+    cnumbers = []
+    for obj in resultObjList:
+        qnumbers.append(int(obj['qnumber']))
+        cnumbers.append(int(obj['cnumber']))
 
     # Creates anonymous profile for each submission
     profile_id = db.createProfile()
@@ -71,20 +81,100 @@ def post_survey():
     # Creates anonymous profile for each submission
     sub_id = db.createSurveySubmission(survey_id, profile_id)
     print("created SurveySubmission")
-
+ 
     # qnumbers, cnumbers = zip(*data['results'])
-    qnumbers = [12, 13]
-    cnumbers = [3, 4]
+    # qnumbers = [12, 13]
+    # cnumbers = [3, 4]
     # Populates Responses Table
     db.insertResponses(sub_id, qnumbers, cnumbers)
     print("inserted Responses")
 
     # Populates DescriptorScores Tables
-    db.insertDescriptorScores(profile_id, qnumbers, cnumbers, survey_id)
+    db.insertDescriptorScores(profile_id, qnumbers, cnumbers, survey_id) 
     print("inserted DescriptorScores")
 
+    # Executes any Select Query
+    def executeSelect(query):
+        connection = pymysql.connect(
+                    user     = "group5a",
+                    password = "_}2nSW6%?3hyr9Z",
+                    host     = "mysql.labthreesixfive.com",
+                    db       = "group5a",
+                    port     = 3306
+        )
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+        connection.commit()
+        return cursor.fetchall()
 
-    # TODO : Call recomendation engine and return 
-    response = {}
+    def get_onet_profiles():
+        query = """SELECT Id FROM Profiles WHERE Category = 'ONET' """
+        ids = executeSelect(query)
+        return ids
+
+    # """
+    # WITH t1 AS (SELECT JobDescriptorId, Score from DescriptorScores where ProfileId = 1 order by JobDescriptorId),
+    # t2 as (SELECT JobDescriptorId, Score from DescriptorScores where ProfileId = 287 order by JobDescriptorId)
+    # SELECT * FROM t1 JOIN t2 ON JobDescriptorId ORDER BY JobDescriptorId;
+    # """
+
+    # for every ONET job
+    onet_job_ids = get_onet_profiles()
+    onet_cosine_similarity = {}
+    for job_id in onet_job_ids: 
+        query = f"""
+            WITH t1 AS (SELECT JobDescriptorId, Score FROM DescriptorScores WHERE ProfileId = {profile_id}),
+            t2 AS (SELECT JobDescriptorId, Score FROM DescriptorScores WHERE ProfileId = {job_id})
+            SELECT t1.JobDescriptorId, t1.Score, t2.Score FROM t1 JOIN t2 ON t1.JobDescriptorId = t2.JobDescriptorId ORDER BY t1.JobDescriptorId;
+            """ # first col is experience profile, second col is onet job profile
+
+        job_descriptor_ids, experience_scores, onet_scores = zip(*executeSelect(query)) # executeSelect returns [(1, 6, 7), ...]
+        
+        experience_scores_list = list(experience_scores)
+        onet_scores_list = list(onet_scores)
+
+        similarity_score = 1 - spatial.distance.cosine(experience_scores_list, onet_scores_list)
+        onet_cosine_similarity[job_id] = similarity_score # dictionary: onetjobid --> similarity score 
+        
+    #take top 10 values from onet_cosine_similarity
+    #RESOURCE: https://www.geeksforgeeks.org/python-n-largest-values-in-dictionary/
+    N = 10
+    results = dict(sorted(onet_cosine_similarity.items(), key = itemgetter(1), reverse = True)[:N]) #key : profileid, value: similarity score
+
+    # get names of recommended jobs
+    recommended_job_list = []
+    for job_id in results.keys():
+
+        query = f"""
+            SELECT Name
+            FROM Profiles
+            WHERE Id = {job_id};
+            """
+        job_name = executeSelect(query)[0] # (Name)
+
+        query = f"""
+            SELECT Code
+            FROM Profiles
+            WHERE Id = {job_id};
+        """
+        job_code = executeSelect(query)[0] # (Code)
+
+        query = f"""
+            SELECT Description
+            FROM ONETJobInfo
+            WHERE Code = {job_code};
+        """
+        job_desc = executeSelect(query)[0] # (Desc)
+
+        job_obj = {
+            "job_id": job_id,
+            "job_code": job_code,
+            "job_name": job_name,
+            "job_desc": job_desc,
+            "cos_similarity": onet_cosine_similarity[job_id]
+        }
+
+        recommended_job_list.append(job_obj)
     
-    return jsonify(response)
+    return jsonify(recommended_job_list) # TODO: does this need to be jsonified?
+
