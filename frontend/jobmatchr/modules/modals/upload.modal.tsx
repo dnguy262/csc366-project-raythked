5500629015711085f48224371e489a7c7c77af84
Jin Choi <jinyoungchoi118@gmail.com>
2022-06-06 21:51:29 -0700

    updated readme and addressed some lint issues

--- frontend/jobmatchr/modules/modals/upload.modal.tsx
+++ frontend/jobmatchr/modules/modals/upload.modal.tsx
@@ -2,3 +2,5 @@
 
 // actual support of file uploading and parsing can be complicated on the backend side so this is a stretch goal
 // that we will likely not reach with how much time we have left.
+
+export {};
